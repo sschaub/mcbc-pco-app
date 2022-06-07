99a6f7d71933c0dd685d2429d0ff3f3ab5f3a310
Stephen Schaub <sschaub@bju.edu>
2022-06-07 18:33:01 -0400

    Add note categories for special service

--- api/const.py
+++ api/const.py
@@ -23,16 +23,19 @@ PLAN_DETAIL_URL = '/services/v2/service_types/{}/plans/{}'
 ITEM_DETAIL_URL = '/services/v2/service_types/{}/plans/{}/items/{}'
 # The ID numbers in the following come from the PCO API https://api.planningcenteronline.com/services/v2/service_types
 SERVICE_TYPES = { 1060129: 'Sunday AM', 1060132: 'Sunday PM', 1060133: 'Wednesday', 1243611: 'Special Service' }
+# The note categories come from PCO API https://api.planningcenteronline.com/services/v2/service_types/(service_type_id)/item_note_categories
 SERVICE_ORDER_NOTE_CATEGORIES = { 
     # service_order_id: item_note_category_id
     1060129: 3987812, # Sunday AM
     1060132: 3993389, # Sunday PM
-    1060133: 3993728  # Wednesday
+    1060133: 3993728, # Wednesday
+    1243611: 4717356  # Special service
     }
 LOCATION_NOTE_CATEGORIES = { 
     1060129: 3992309,  # Sunday AM
     1060132: 3993388,  # Sunday PM
-    1060133: 3993729   # Wednesday
+    1060133: 3993729,  # Wednesday
+    1243611: 4717357   # Special service
     }
 EDITABLE_ITEMS = ['Prelude Opener', 'Vocal Special', 'Service Opener', 'Instrumental Special', 'Organ Special', 'Offertory']
 SERVICE_POSITIONS = ['Songleader', 'Organ', 'Piano', 'Piano 2']
